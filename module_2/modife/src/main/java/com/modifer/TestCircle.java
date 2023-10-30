e75a12a0c5c3feb19a13eab554550b82d7769ae5
Long13062005 <vulong13062005@gmail.com>
2023-10-30 21:18:56 +0700

    s

---
+++ module_2/modife/src/main/java/com/modifer/TestCircle.java
@@ -0,0 +1,11 @@
+package com.modifer;
+
+import main.java.com.modifer.Circle;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+    }
+}
