9444b0f72b0f1a8c07286450da893991c9a2f78c
Long13062005 <vulong13062005@gmail.com>
2023-10-30 21:19:31 +0700

    s

---
+++ module_2/modife/src/main/resources/Test.java
@@ -0,0 +1,9 @@
+package main.resources;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.setName("alo"));
+        System.out.println(student.setClasses("C2"));
+    }
+}
